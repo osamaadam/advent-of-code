833f66e41a3d9dd2876981682889aa3c35f78028
Osama Adam <osamaadamme@gmail.com>
2024-03-30 02:02:54 +0200

    day 3 part 1

---
+++ 2023/3/index.mjs
@@ -0,0 +1,68 @@
+import { parseInput } from "../util/parseInput.mjs";
+
+const lines = await parseInput("input.txt");
+const matrix = lines.map((line) => line.split(""));
+
+function isPartNumber(row, col) {
+  const adjacentCells = [
+    [row + 1, col],
+    [row - 1, col],
+    [row, col + 1],
+    [row, col - 1],
+    [row + 1, col + 1],
+    [row + 1, col - 1],
+    [row - 1, col + 1],
+    [row - 1, col - 1],
+  ].filter(
+    ([r, c]) => r >= 0 && r < matrix.length && c >= 0 && c < matrix.length,
+  );
+
+  for (const [r, c] of adjacentCells) {
+    const cell = matrix[r][c];
+    if (isNaN(cell) && cell !== ".") {
+      // Not a number and not a dot
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function findPartNumbers() {
+  const partNumbers = [];
+  let curNumber = [];
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col <= matrix.length; col++) {
+      if (isNaN(matrix[row][col]) || col === matrix.length) {
+        if (curNumber.length > 0) {
+          const isCurPartNumber = curNumber.reduce(
+            (prev, cur) => prev || isPartNumber(cur[0], cur[1]),
+            false,
+          );
+          if (isCurPartNumber) {
+            const partNumber = parseInt(
+              curNumber.reduce(
+                (prev, cur) => prev + matrix[cur[0]][cur[1]],
+                "",
+              ),
+              10,
+            );
+
+            partNumbers.push(partNumber);
+          }
+        }
+        curNumber = [];
+        continue;
+      }
+      curNumber.push([row, col]);
+    }
+  }
+
+  return partNumbers;
+}
+
+const partNumbers = findPartNumbers();
+
+const partOneResult = partNumbers.reduce((prev, cur) => prev + cur, 0);
+
+console.log(`The answer to part 1 is: ${partOneResult}`);
