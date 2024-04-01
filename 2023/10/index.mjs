e91f3d879cb9e9c4d04c3c67c94badbce97c07d0
Osama Adam <osamaadamme@gmail.com>
2024-04-02 00:37:20 +0200

    day 10 part 2 close

--- 2023/10/index.mjs
+++ 2023/10/index.mjs
@@ -46,7 +46,7 @@ function navigateLoop(startingPos, matrix) {
       if (
         nextR >= matrix.length ||
         nextR < 0 ||
-        nextC >= matrix.length ||
+        nextC >= matrix[0].length ||
         nextC < 0 ||
         visited.has(`${nextR},${nextC}`)
       ) {
@@ -100,3 +100,82 @@ function navigateLoop(startingPos, matrix) {
 const visitedCells = navigateLoop(findStartingPos(matrix), matrix);
 
 console.log(`The solution to part one is: ${visitedCells.size / 2}`);
+
+/**
+ * @param {number[]} startingPos
+ * @param {string} direction
+ */
+function castRay(startingPos, direction) {
+  const inversions = [];
+  const countingBarriers = ["J", "L", "F", "7", "S"];
+  switch (direction) {
+    case "NORTH":
+    case "SOUTH":
+      countingBarriers.push("-");
+      inversions.push("J", "F");
+      break;
+    case "WEST":
+    case "EAST":
+      countingBarriers.push("|");
+      inversions.push("L", "7");
+      break;
+  }
+
+  let barrierCount = 0;
+  let inversionsCount = 0;
+  const [dr, dc] = pm[direction];
+
+  while (
+    startingPos[0] < matrix.length &&
+    startingPos[0] >= 0 &&
+    startingPos[1] < matrix[0].length &&
+    startingPos[1] >= 0
+  ) {
+    const curElem = matrix[startingPos[0]][startingPos[1]];
+    if (
+      countingBarriers.includes(curElem) &&
+      visitedCells.has(`${startingPos[0]},${startingPos[1]}`)
+    ) {
+      if (inversions.includes(curElem)) {
+        inversionsCount++;
+      } else {
+        barrierCount++;
+      }
+    }
+
+    startingPos = [startingPos[0] + dr, startingPos[1] + dc];
+  }
+
+  return barrierCount + Math.floor(inversionsCount / 2);
+}
+
+let enclosedTiles = 0;
+
+for (let r = 0; r < matrix.length; r++) {
+  for (let c = 0; c < matrix[0].length; c++) {
+    let isEnclosed = true;
+    for (const [dir, delta] of Object.entries(pm)) {
+      const [dr, dc] = delta;
+      const [curR, curC] = [r + dr, c + dc];
+      if (
+        curR >= matrix.length ||
+        curR < 0 ||
+        curC >= matrix[0].length ||
+        curC < 0
+      ) {
+        isEnclosed = false;
+        break;
+      }
+      const barriers = castRay([r, c], dir);
+      if (barriers % 2 === 0) {
+        isEnclosed = false;
+        break;
+      }
+    }
+    if (isEnclosed) {
+      enclosedTiles++;
+    }
+  }
+}
+
+console.log({ enclosedTiles });
