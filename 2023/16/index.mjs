5d3edcd6a1990326017eec79ebdc3d1c952606e3
Osama Adam <osamaadamme@gmail.com>
2024-04-06 20:33:01 +0200

    day 16 part 1

--- 2023/16/index.mjs
+++ 2023/16/index.mjs
@@ -1,126 +1,105 @@
-import { readFileSync } from "fs";
+import { parseInput } from "../util/parseInput.mjs";
+
+const lines = await parseInput("input.txt");
+
+const matrix = lines.map((line) => line.split(""));
 
 const DIRECTIONS = {
-  NORTH: [0, -1],
-  SOUTH: [0, 1],
-  EAST: [1, 0],
-  WEST: [-1, 0],
-};
-
-const POINTING_DIRECTIONS = {
-  NORTH: "NORTH",
-  SOUTH: "SOUTH",
-  EAST: "EAST",
-  WEST: "WEST",
+  NORTH: [-1, 0],
+  SOUTH: [1, 0],
+  EAST: [0, 1],
+  WEST: [0, -1],
 };
 
 const MIRRORS = {
-  "/": {
-    NORTH: [DIRECTIONS.WEST],
-    EAST: [DIRECTIONS.SOUTH],
-    SOUTH: [DIRECTIONS.EAST],
-    WEST: [DIRECTIONS.NORTH],
-  },
-  ")": {
-    NORTH: [DIRECTIONS.EAST],
-    EAST: [DIRECTIONS.NORTH],
-    SOUTH: [DIRECTIONS.WEST],
-    WEST: [DIRECTIONS.SOUTH],
-  },
   "|": {
-    NORTH: [DIRECTIONS.SOUTH],
     EAST: [DIRECTIONS.NORTH, DIRECTIONS.SOUTH],
-    SOUTH: [DIRECTIONS.NORTH],
     WEST: [DIRECTIONS.NORTH, DIRECTIONS.SOUTH],
   },
   "-": {
-    NORTH: [DIRECTIONS.WEST, DIRECTIONS.EAST],
-    EAST: [DIRECTIONS.WEST],
-    SOUTH: [DIRECTIONS.WEST, DIRECTIONS.EAST],
-    WEST: [DIRECTIONS.EAST],
+    NORTH: [DIRECTIONS.EAST, DIRECTIONS.WEST],
+    SOUTH: [DIRECTIONS.EAST, DIRECTIONS.WEST],
   },
-  ".": {
-    NORTH: [DIRECTIONS.SOUTH],
-    EAST: [DIRECTIONS.WEST],
-    SOUTH: [DIRECTIONS.NORTH],
-    WEST: [DIRECTIONS.EAST],
+  "/": {
+    NORTH: [DIRECTIONS.EAST],
+    SOUTH: [DIRECTIONS.WEST],
+    EAST: [DIRECTIONS.NORTH],
+    WEST: [DIRECTIONS.SOUTH],
+  },
+  "\\": {
+    NORTH: [DIRECTIONS.WEST],
+    SOUTH: [DIRECTIONS.EAST],
+    EAST: [DIRECTIONS.SOUTH],
+    WEST: [DIRECTIONS.NORTH],
   },
 };
 
-const input = readFileSync("./input.txt", "utf8").trim().replaceAll("\\", ")");
-// const input = String.raw`
-// .|...\....
-// |.-.\.....
-// .....|-...
-// ........|.
-// ..........
-// .........\
-// ..../.\\..
-// .-.-/..|..
-// .|....-|.\
-// ..//.|....`
-//   .replaceAll("\\", ")")
-//   .trim();
+const visited = new Set();
 
-const rows = parseText(input);
-
-console.log(rows);
-
-const energyMatrix = rows.map((row) => row.map(() => new Set()));
-
-propagateEnergy();
-
-console.log(`The solution to the first part is ${calculateEnergizedCells()}`);
-
-function parseText(input) {
-  return input.split("\n").map((row) => row.split(""));
-}
-
-function stringifyRows(rows) {
-  return rows
-    .map((row) => row.join(""))
-    .join("\n")
-    .trim();
-}
-
-function propagateEnergy(curr = [0, 0], prevMove = [0, 0]) {
-  const [x, y] = curr;
-
-  let comingFrom = null;
-
-  const guess = [x - prevMove[0], y - prevMove[1]];
-
-  if (guess[0] === 0 && guess[1] === 1) {
-    comingFrom = POINTING_DIRECTIONS.NORTH;
-  } else if (guess[0] === 0 && guess[1] === -1) {
-    comingFrom = POINTING_DIRECTIONS.SOUTH;
-  } else if (guess[0] === -1 && guess[1] === 0) {
-    comingFrom = POINTING_DIRECTIONS.EAST;
-  } else {
-    comingFrom = POINTING_DIRECTIONS.WEST;
-  }
+/**
+ * @param {number[]} pos
+ * @param {number[]} dir
+ * @param {string[][]} matrix
+ * @param {number[][]} illuMatrix
+ *
+ * @returns {number[][]}
+ */
+function navigate(pos, dir, matrix, illuMatrix) {
+  const [row, col] = [pos[0] + dir[0], pos[1] + dir[1]];
+  const key = `${[row, col]},${dir}`;
 
   if (
-    x >= rows[0].length ||
-    y >= rows.length ||
-    x < 0 ||
-    y < 0 ||
-    energyMatrix[y][x].has(comingFrom)
+    row >= matrix.length ||
+    row < 0 ||
+    col >= matrix[row].length ||
+    col < 0 ||
+    visited.has(key)
   ) {
-    return;
+    return illuMatrix;
   }
-  energyMatrix[y][x].add(comingFrom);
 
-  const nextMoves = MIRRORS[rows[y][x]][comingFrom];
+  visited.add(key);
+  illuMatrix[row][col] += 1;
 
-  for (const nextMove of nextMoves) {
-    const [dx, dy] = nextMove;
-    propagateEnergy([x + dx, y + dy], curr);
+  const curElem = matrix[row][col];
+  const curDir = Object.entries(DIRECTIONS).find(
+    ([_, [y, x]]) => y === dir[0] && x === dir[1],
+  )[0];
+
+  if (curElem in MIRRORS) {
+    const nextMoves = MIRRORS[curElem][curDir];
+    if (!nextMoves) {
+      return navigate([row, col], dir, matrix, illuMatrix);
+    }
+    for (const move of nextMoves) {
+      illuMatrix = navigate([row, col], move, matrix, illuMatrix);
+    }
+    return illuMatrix;
+  }
+
+  return navigate([row, col], dir, matrix, illuMatrix);
+}
+
+let illuMatrix = Array.from({ length: matrix.length }, () =>
+  Array.from({ length: matrix[0].length }, () => 0),
+);
+
+illuMatrix[0][0] = 1;
+illuMatrix[1][0] = 1;
+
+// Noticed that in my input the entry point is actually a mirror
+// So I decided it was easier if I redirect it manually in the beginning.
+illuMatrix = navigate([1, 0], DIRECTIONS.SOUTH, matrix, illuMatrix);
+
+let partOneSolution = 0;
+
+for (let row = 0; row < illuMatrix.length; row++) {
+  for (let col = 0; col < illuMatrix[row].length; col++) {
+    const elem = illuMatrix[row][col];
+    if (elem >= 1) {
+      partOneSolution++;
+    }
   }
 }
 
-function calculateEnergizedCells() {
-  return energyMatrix.reduce((outerAcc, row) => {
-    return outerAcc + row.reduce((acc, cell) => acc + (cell.size ? 1 : 0), 0);
-  }, 0);
-}
+console.log(`The solution to part one is: ${partOneSolution}`);
