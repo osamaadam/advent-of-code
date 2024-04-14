b09d5177e2fde27d56696f193363756d6816ea54
Osama Adam <osamaadamme@gmail.com>
2024-04-14 07:22:54 +0200

    2022 day 10 part 2, breaking part 1

--- 2022/10/index.mjs
+++ 2022/10/index.mjs
@@ -12,19 +12,32 @@ const instrs = lines.map((line) => {
 let cycle = 0;
 let x = 1;
 let signalPower = 0;
-const processedSet = new Set();
+let curRow = -1;
+const crt = Array.from({ length: 6 }, () =>
+  Array.from({ length: 40 }, () => "░"),
+);
 
-function calculateSignalPower() {
-  if (
-    [20, 60, 100, 140, 180, 220].includes(cycle) &&
-    !processedSet.has(cycle)
-  ) {
-    signalPower += x * cycle;
-    processedSet.add(cycle);
+function renderCRT() {
+  const spritePos = x - 1;
+  const curCol = cycle % 40;
+  if (curCol === 0) {
+    curRow++;
+  }
+
+  if (curCol >= spritePos && curCol < spritePos + 3) {
+    crt[curRow][curCol] = "█";
   }
 }
 
+function calculateSignalPower() {
+  if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
+    signalPower += x * cycle;
+  }
+  renderCRT();
+}
+
 for (const { instr, arg } of instrs) {
+  calculateSignalPower();
   switch (instr) {
     case "noop":
       cycle++;
@@ -33,11 +46,10 @@ for (const { instr, arg } of instrs) {
       cycle++;
       calculateSignalPower();
       cycle++;
-      calculateSignalPower();
       x += arg;
       break;
   }
-  calculateSignalPower();
 }
 
 console.log(`The solution to part one is: ${signalPower}`);
+console.log(crt.map((row) => row.join("")).join("\n"));
