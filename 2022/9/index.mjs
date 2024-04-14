721d1bebc5a9542b39a99d42244412d30ac6f63f
Osama Adam <osamaadamme@gmail.com>
2024-04-14 06:15:45 +0200

    2022 day 9 part 2 attempt

--- 2022/9/index.mjs
+++ 2022/9/index.mjs
@@ -7,55 +7,66 @@ const instructions = lines.map((line) => {
   return [dir, Number(rep)];
 });
 
-const visitedCells = new Set(["0,0"]);
+function move(knots = 1) {
+  const visitedCells = new Set(["0,0"]);
 
-let hPos = [0, 0];
-let tPos = [0, 0];
+  const tailsPos = Array.from({ length: knots + 1 }, () => null).map(() => [
+    0, 0,
+  ]);
 
-for (let [dir, rep] of instructions) {
-  let move;
-  switch (dir) {
-    case "U":
-      move = [0, -1];
-      break;
-    case "R":
-      move = [1, 0];
-      break;
-    case "D":
-      move = [0, 1];
-      break;
-    case "L":
-      move = [-1, 0];
-      break;
+  for (let [dir, rep] of instructions) {
+    let move;
+    switch (dir) {
+      case "U":
+        move = [0, -1];
+        break;
+      case "R":
+        move = [1, 0];
+        break;
+      case "D":
+        move = [0, 1];
+        break;
+      case "L":
+        move = [-1, 0];
+        break;
+    }
+
+    for (let i = 0; i < rep; i++) {
+      moveHead(move, knots);
+    }
   }
 
-  for (let i = 0; i < rep; i++) {
-    moveHead(move);
-    moveTail(move);
-  }
-}
-
-/**
- * @param {number[]} move
- */
-function moveHead(move) {
-  const [r, c] = move;
-  hPos = [hPos[0] + r, hPos[1] + c];
-}
-
-/**
- * @param {number[]} move
- */
-function moveTail(move) {
-  const [r, c] = move;
-  const [hR, hC] = hPos;
-  const [tR, tC] = tPos;
-  if (Math.abs(hR - tR) <= 1 && Math.abs(hC - tC) <= 1) {
-    return;
+  /**
+   * @param {number[]} move
+   */
+  function moveHead(move) {
+    const [r, c] = move;
+    const hPos = tailsPos[0];
+    tailsPos[0] = [hPos[0] + r, hPos[1] + c];
+    moveTail(1, hPos);
   }
 
-  tPos = [hR - r, hC - c];
-  visitedCells.add(`${tPos[0]},${tPos[1]}`);
+  function moveTail(index, prev) {
+    if (index > knots) {
+      return;
+    }
+    const [hR, hC] = tailsPos[index - 1];
+    const [tR, tC] = tailsPos[index];
+    if (Math.abs(hR - tR) <= 1 && Math.abs(hC - tC) <= 1) {
+      return;
+    }
+
+    const tailPrev = [...tailsPos[index]];
+    tailsPos[index] = prev;
+    if (index === knots) {
+      const tPos = tailsPos[index];
+      visitedCells.add(`${tPos[0]},${tPos[1]}`);
+    }
+    moveTail(index + 1, tailPrev);
+  }
+
+  return visitedCells.size;
 }
 
-console.log(`The solution to part one is: ${visitedCells.size}`);
+console.log(`The solution to part one is: ${move(1)}`);
+console.log(`The solution to part two is: ${move(10)}`);
