42c5245a923e39977be1ff976cb28ac87713dd01
Osama Adam <osamaadamme@gmail.com>
2024-04-23 18:29:08 +0200

    2021 day 5 part 2

--- 2021/5/index.mjs
+++ 2021/5/index.mjs
@@ -3,25 +3,34 @@ import { parseInput } from "../../2023/util/parseInput.mjs";
 const lines = await parseInput("input.txt");
 
 const ints = [];
-let rows = 0;
-let cols = 0;
+const diagonals = [];
+let rows = 1;
+let cols = 1;
 
 for (const line of lines) {
   const [int1, int2] = line.split(" -> ");
   const [x1, y1] = int1.trim().split(",").map(Number);
   const [x2, y2] = int2.trim().split(",").map(Number);
 
+  rows = Math.max(rows, Math.max(x1 + 1, x2 + 1));
+  cols = Math.max(cols, Math.max(y1 + 1, y2 + 1));
+
+  const slope = (x2 - x1) / (y2 - y1);
+  const int = [
+    [x1, y1],
+    [x2, y2],
+  ].sort((a, b) => a[0] - b[0]);
+
+  if (slope === 1 || slope === -1) {
+    diagonals.push(int);
+    continue;
+  }
+
   if (x1 !== x2 && y1 !== y2) {
     continue;
   }
 
-  rows = Math.max(rows, Math.max(x1 + 1, x2 + 1));
-  cols = Math.max(cols, Math.max(y1 + 1, y2 + 1));
-
-  ints.push([
-    [x1, y1],
-    [x2, y2],
-  ]);
+  ints.push(int);
 }
 
 const matrix = Array.from({ length: rows }, () =>
@@ -29,15 +38,15 @@ const matrix = Array.from({ length: rows }, () =>
 );
 
 for (const [int1, int2] of ints) {
-  const [c1, r1] = int1;
-  const [c2, r2] = int2;
+  const [x1, y1] = int1;
+  const [x2, y2] = int2;
 
-  const rStart = Math.min(r1, r2);
-  const rEnd = Math.max(r1, r2);
-  const cStart = Math.min(c1, c2);
-  const cEnd = Math.max(c1, c2);
-  for (let r = rStart; r <= rEnd; r++) {
-    for (let c = cStart; c <= cEnd; c++) {
+  const yStart = Math.min(y1, y2);
+  const yEnd = Math.max(y1, y2);
+  const xStart = Math.min(x1, x2);
+  const xEnd = Math.max(x1, x2);
+  for (let r = yStart; r <= yEnd; r++) {
+    for (let c = xStart; c <= xEnd; c++) {
       matrix[r][c] += 1;
     }
   }
@@ -53,3 +62,27 @@ for (let r = 0; r < matrix.length; r++) {
 }
 
 console.log(`The solution to the first part is: ${overlaps}`);
+
+for (const [int1, int2] of diagonals) {
+  const [x1, y1] = int1;
+  const [x2, y2] = int2;
+  const slope = (y2 - y1) / (x2 - x1);
+  const span = x2 - x1;
+
+  for (let i = 0; i <= span; i++) {
+    const x = x1 + i;
+    const y = y1 + i * slope;
+    matrix[y][x] += 1;
+  }
+}
+
+overlaps = 0;
+for (let r = 0; r < matrix.length; r++) {
+  for (let c = 0; c <= matrix[r].length; c++) {
+    if (matrix[r][c] > 1) {
+      overlaps++;
+    }
+  }
+}
+
+console.log(`The solution to the second part is: ${overlaps}`);
