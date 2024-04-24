104315165e7e855fb10d3695184e49097ae1bcee
Osama Adam <osamaadamme@gmail.com>
2024-04-24 16:26:55 +0200

    2021 day 10 part 2

--- 2021/10/index.mjs
+++ 2021/10/index.mjs
@@ -50,3 +50,64 @@ const score = lines
   .reduce((prev, cur) => prev + cur, 0);
 
 console.log(`The solution to the first part is: ${score}`);
+
+/**
+ * @param {string} line
+ * @returns {number[]}
+ */
+function findScores(line) {
+  const chars = line.trim().split("");
+  const stack = [];
+  for (const char of chars) {
+    switch (char) {
+      case ")":
+        if (stack.at(-1) !== "(") {
+          return 0;
+        }
+        stack.pop();
+        break;
+      case "]":
+        if (stack.at(-1) !== "[") {
+          return 0;
+        }
+        stack.pop();
+        break;
+      case "}":
+        if (stack.at(-1) !== "{") {
+          return 0;
+        }
+        stack.pop();
+        break;
+      case ">":
+        if (stack.at(-1) !== "<") {
+          return 0;
+        }
+        stack.pop();
+        break;
+      default:
+        stack.push(char);
+        break;
+    }
+  }
+
+  const SCORES = {
+    "(": 1,
+    "[": 2,
+    "{": 3,
+    "<": 4,
+  };
+
+  return stack
+    .map((char) => SCORES[char])
+    .reverse()
+    .reduce((prev, cur) => prev * 5 + cur, 0);
+}
+
+const solution = lines
+  .map(findScores)
+  .filter((num) => num > 0)
+  .sort((a, b) => a - b);
+
+console.log(
+  `The solution to the second part is: ${solution[Math.floor(solution.length / 2)]}`,
+);
