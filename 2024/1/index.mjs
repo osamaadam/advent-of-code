14dd7c9038aaca4e1e2f8827d696e009f9b03f92
Osama Adam <osama.adam@saloodo.com>
2024-12-01 15:49:16 +0200

    2024/1 part 2

--- 2024/1/index.mjs
+++ 2024/1/index.mjs
@@ -4,11 +4,15 @@ const lines = await parseInput("./input.txt");
 
 let left = [];
 let right = [];
+const freq = {};
 
 for (const line of lines) {
   const [l, r] = line.split("  ");
-  left.push(parseInt(l));
-  right.push(parseInt(r));
+  const lInt = parseInt(l);
+  const rInt = parseInt(r);
+  left.push(lInt);
+  right.push(rInt);
+  freq[rInt] = (freq[rInt] || 0) + 1;
 }
 
 left = left.sort((a, b) => a - b);
@@ -21,3 +25,11 @@ for (let i = 0; i < left.length; i++) {
 }
 
 console.log(`The total distance for part 1 is: ${distance}`);
+
+let similarityScore = 0;
+
+for (const l of left) {
+  similarityScore += l * (freq[l] || 0);
+}
+
+console.log(`The similarity score for part 2 is: ${similarityScore}`);
